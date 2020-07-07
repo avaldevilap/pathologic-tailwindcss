b8310b9f69ba0ace422aaea3a94f36d97a55c622
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2020-07-07 09:39:28 -0400

    Modified header

--- components/Header.vue
+++ components/Header.vue
@@ -1,69 +1,261 @@
 <template>
-  <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-    <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <nuxt-link class="font-semibold text-xl tracking-tight app-brand" to="/">
-        Pathologic
-      </nuxt-link>
+  <nav class="bg-teal-500">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <nuxt-link
+              class="font-semibold text-xl text-white tracking-tight app-brand"
+              to="/"
+            >
+              Pathologic
+            </nuxt-link>
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-10 flex items-baseline">
+              <nuxt-link
+                to="/patients"
+                class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
+              >
+                Pacientes
+              </nuxt-link>
+              <a
+                href="#"
+                class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
+              >
+                Biopsias
+              </a>
+              <a
+                href="#"
+                class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
+              >
+                Citologías
+              </a>
+              <a
+                href="#"
+                class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
+              >
+                Necropsias
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="hidden md:block">
+          <div class="ml-4 flex items-center md:ml-6">
+            <button
+              class="p-1 border-2 border-transparent text-teal-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-teal-700"
+              aria-label="Notifications"
+            >
+              <svg
+                class="h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+            </button>
+
+            <!-- Profile dropdown -->
+            <div class="ml-3 relative">
+              <div>
+                <button
+                  id="user-menu"
+                  class="max-w-xs flex items-center text-sm rounded-full text-white focus:outline-none focus:shadow-solid"
+                  aria-label="User menu"
+                  aria-haspopup="true"
+                  @click="profileMenu = !profileMenu"
+                >
+                  <img
+                    class="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </button>
+              </div>
+              <!--
+                Profile dropdown panel, show/hide based on dropdown state.
+
+                Entering: "transition ease-out duration-100"
+                  From: "transform opacity-0 scale-95"
+                  To: "transform opacity-100 scale-100"
+                Leaving: "transition ease-in duration-75"
+                  From: "transform opacity-100 scale-100"
+                  To: "transform opacity-0 scale-95"
+              -->
+              <transition
+                enter-active-class="transition ease-out duration-100"
+                enter-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <div
+                  v-show="profileMenu"
+                  class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg"
+                >
+                  <div
+                    class="py-1 rounded-md bg-white shadow-xs"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu"
+                  >
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      role="menuitem"
+                    >
+                      Perfil
+                    </a>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      role="menuitem"
+                    >
+                      Opciones
+                    </a>
+                    <a
+                      class="block px-4 py-2 text-sm text-gray-700 cursor-pointer hover:bg-gray-100"
+                      role="menuitem"
+                      @click="$auth.logout()"
+                    >
+                      Salir
+                    </a>
+                  </div>
+                </div>
+              </transition>
+            </div>
+          </div>
+        </div>
+        <div class="-mr-2 flex md:hidden">
+          <!-- Mobile menu button -->
+          <button
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-teal-700 focus:outline-none focus:bg-teal-700 focus:text-white"
+            @click="mobileMenu = !mobileMenu"
+          >
+            <!-- Menu open: "hidden", Menu closed: "block" -->
+            <svg
+              class="block h-6 w-6"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+            <!-- Menu open: "block", Menu closed: "hidden" -->
+            <svg
+              class="hidden h-6 w-6"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
 
-    <div class="block lg:hidden">
-      <button
-        class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-      >
-        <svg
-          class="fill-current h-3 w-3"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </button>
-    </div>
+    <!--
+      Mobile menu, toggle classes based on menu state.
 
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-      <div class="text-sm lg:flex-grow">
+      Open: "block", closed: "hidden"
+    -->
+    <div :class="[!mobileMenu ? 'hidden' : 'block', 'md:hidden']">
+      <div class="px-2 pt-2 pb-3 sm:px-3">
         <nuxt-link
           to="/patients"
-          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
         >
           Pacientes
         </nuxt-link>
-        <nuxt-link
-          to="/biopsies"
-          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        <a
+          href="#"
+          class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
         >
           Biopsias
-        </nuxt-link>
+        </a>
         <a
-          href="#responsive-header"
-          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          href="#"
+          class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
         >
           Citologías
         </a>
         <a
-          href="#responsive-header"
-          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+          href="#"
+          class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
         >
           Necropsias
         </a>
       </div>
-    </div>
-
-    <template v-if="$auth.loggedIn">
-      <div class="block">
-        <p-button
-          label="Salir"
-          class="bg-teal-600 hover:bg-teal-700"
-          @click="$auth.logout()"
-        />
+      <div class="pt-4 pb-3 border-t border-gray-700">
+        <div class="flex items-center px-5">
+          <div class="flex-shrink-0">
+            <img
+              class="h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </div>
+          <div class="ml-3">
+            <div class="text-base font-medium leading-none text-white">
+              Tom Cook
+            </div>
+            <div class="mt-1 text-sm font-medium leading-none text-gray-400">
+              tom@example.com
+            </div>
+          </div>
+        </div>
+        <div class="mt-3 px-2">
+          <a
+            href="#"
+            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
+          >
+            Perfil
+          </a>
+          <a
+            href="#"
+            class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
+          >
+            Opciones
+          </a>
+          <a
+            class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 cursor-pointer hover:text-white hover:bg-teal-700 focus:outline-none focus:text-white focus:bg-teal-700"
+            @click="$auth.logout()"
+          >
+            Salir
+          </a>
+        </div>
       </div>
-    </template>
+    </div>
   </nav>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      mobileMenu: false,
+      profileMenu: false,
+    };
+  },
+});
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,12 @@
   <div>
     <Header />
 
-    <div class="container mt-5">
-      <Nuxt />
-    </div>
+    <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="px-4 py-6 sm:px-0">
+          <Nuxt />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
